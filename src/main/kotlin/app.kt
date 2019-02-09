1bb2fb3d8b53e52332be1f9a4cb85aa364374569
Miguel Angel Cañas Vaz <mac@tid.es>
2019-02-09 21:14:14 +0100

    Working compilation

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -2,7 +2,10 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
-fun main(args: Array<String>) {
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+fun main2(args: Array<String>) {
     println("Hello world!")
     val db = Database.connect(
         "jdbc:postgresql://localhost:5432/extraction",
@@ -15,4 +18,13 @@ fun main(args: Array<String>) {
         }
     }
     println("Connected to DB")
+}
+
+
+
+@SpringBootApplication
+class ProvisionAPIApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProvisionAPIApplication>(*args)
 }
